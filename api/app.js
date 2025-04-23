f1ec40118c7dc4a3923e715b295f34a9a066cb67
Talys Freitas <112765113+BanLuck@users.noreply.github.com>
2025-04-23 20:39:51 -0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,9 +1,12 @@
+//Adiciona após a conexão com o MongoDB
 const express = require('express');
 const authRoutes = require('./routes/authRoutes.js');
+const postRoutes = require('./routes/postRoutes.js');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 module.exports = app;
